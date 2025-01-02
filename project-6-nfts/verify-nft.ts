adb4c1b2cc6364877a89ed716c1afce648936df1
Santiago Marulanda <santiagomm1997@gmail.com>
2025-01-02 21:40:59 +0100

    Project 6 - nfts

---
+++ project-6-nfts/verify-nft.ts
@@ -0,0 +1,65 @@
+import {
+  createNft,
+  fetchDigitalAsset,
+  findMetadataPda,
+  mplTokenMetadata,
+  verifyCollectionV1,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+
+import { Connection, LAMPORTS_PER_SOL, clusterApiUrl } from "@solana/web3.js";
+import {
+  generateSigner,
+  keypairIdentity,
+  percentAmount,
+  publicKey,
+} from "@metaplex-foundation/umi";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+const user = await getKeypairFromFile();
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL
+);
+
+console.log("Loaded user", user.publicKey.toBase58());
+
+const umi = createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("Set up Umi instance for user");
+
+const collectionAddress = publicKey("A3nEFxmtpHR16YUKKHAC6Etm87wi77SQZYQNArKU8XUr");
+const nftAddress = publicKey("9p2nF9Pc6fboPuY48fZmWG7KmZe5yY2jWyq4fJRxxDZP");
+
+
+const mint = generateSigner(umi);
+
+
+const transaction = await verifyCollectionV1(umi, {
+  metadata: findMetadataPda(umi, { mint: nftAddress }),
+  collectionMint: collectionAddress,
+  authority: umi.identity
+});
+
+transaction.sendAndConfirm(umi);
+
+console.log(
+  `NFT ${nftAddress} verified as member of collection ${collectionAddress}!
+  See explorer at: ${getExplorerLink('address', nftAddress, 'devnet')}
+  `
+)
